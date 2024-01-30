f8e20e2648943d91d7c2c000b8b5a90b4414ab3d
minseok <kms278711@naver.com>
2024-01-30 10:50:40 +0900

    feat : 회원가입 로직 log 추가

--- Easysign_be/src/main/java/com/ssafy/easysign/user/controller/UserController.java
+++ Easysign_be/src/main/java/com/ssafy/easysign/user/controller/UserController.java
@@ -2,12 +2,17 @@ package com.ssafy.easysign.user.controller;
 
 import com.ssafy.easysign.user.dto.request.RegistRequest;
 import com.ssafy.easysign.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/user")
 public class UserController {
 
@@ -20,6 +25,7 @@ public class UserController {
             // 사용자 등록 서비스 호출
             userService.registerUser(registRequest);
             // 성공 시 200 OK 응답
+            log.info("request : " + registRequest);
             return new ResponseEntity<>("User registered successfully!", HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             // 실패 시 400 Bad Request 응답
