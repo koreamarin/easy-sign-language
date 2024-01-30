1bcdb86b9333c3781c8d722f6a89b91f76eb07cb
koreamarin <awldnjs2@naver.com>
2024-01-30 18:01:02 +0900

    commit

--- Easysign_fe-edu/src/App.tsx
+++ Easysign_fe-edu/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import CounterContainer from "./containers/CounterContainer";
+import TodosContainer from "./containers/TodosContainer";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <CounterContainer />
+      -----------------
+      <TodosContainer />
+    </div>
+  );
 }
-
 export default App;
--- Easysign_fe-edu/src/index.tsx
+++ Easysign_fe-edu/src/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import store from "./store/store";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
