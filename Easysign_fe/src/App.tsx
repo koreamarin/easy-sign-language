47964f78a1c9e6e77f8ad06d3ba57168a1a1f33f
서민주 <sminju1009@gmail.com>
2024-01-31 12:06:56 +0900

    fe: axios 설치 및 sidebar 임시 작업

--- Easysign_fe/src/App.tsx
+++ Easysign_fe/src/App.tsx
@@ -3,23 +3,49 @@ import MainPage from "./components/common/MainPage";
 import Login from "./components/common/Login";
 import Join from "./components/common/Join";
 import Navbar from "./components/common/Navbar";
+import Sidebar from "./components/common/Sidebar";
 import StoreMain from "./components/store/StoreMain";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Counter } from "./features/counter/Counter";
+import styled from "styled-components";
 import "./App.css";
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const SidebarContainer = styled.div`
+  text-align: center;
+  width: 357px;
+  flex-direction: column;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex: 1;
+`;
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <AppContainer>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/store" element={<StoreMain />} />
-        </Routes>
-      </div>
+        <MainContent>
+          <SidebarContainer>
+            <Sidebar />
+          </SidebarContainer>
+          <div>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/join" element={<Join />} />
+              <Route path="/store" element={<StoreMain />} />
+            </Routes>
+          </div>
+        </MainContent>
+      </AppContainer>
     </BrowserRouter>
   );
 }
