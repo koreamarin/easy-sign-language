2ecbea46bfaf8c4a7f784543ec1125472ae97da1
kimdohyeon <minalov2@naver.com>
2024-02-05 09:58:19 +0900

    Test : front-edu 연결 테스트

--- Easysign_be/src/main/java/com/ssafy/easysign/user/controller/UserController.java
+++ Easysign_be/src/main/java/com/ssafy/easysign/user/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private UserService userService;
