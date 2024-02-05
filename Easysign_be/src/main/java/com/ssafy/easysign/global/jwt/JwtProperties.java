6e75ea700f1a35263ab22cf0c589a6adf720c721
kimdohyeon <minalov2@naver.com>
2024-02-05 13:12:20 +0900

    fix : JwtProperties  - EXPIRATION_TIME

--- Easysign_be/src/main/java/com/ssafy/easysign/global/jwt/JwtProperties.java
+++ Easysign_be/src/main/java/com/ssafy/easysign/global/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.ssafy.easysign.global.jwt;
 
 public interface JwtProperties {
     String SECRET = "EasySign"; // 우리 서버만 알고 있는 비밀값
-    int EXPIRATION_TIME = 60000*10; // 10일 (1/1000초)
+    int EXPIRATION_TIME = 60000*10*3; // 30분 [ms]
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
