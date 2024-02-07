2e816cc9d2d9c92f87d17439c680436dd5735bef
kimdohyeon <minalov2@naver.com>
2024-02-07 16:03:50 +0900

    fix : 수화 카테고리 가져오기 api 기능 수정

--- Easysign_be/src/main/java/com/ssafy/easysign/sign/dto/response/CategoryResponse.java
+++ Easysign_be/src/main/java/com/ssafy/easysign/sign/dto/response/CategoryResponse.java
@@ -1,6 +1,13 @@
 package com.ssafy.easysign.sign.dto.response;
 
+import com.ssafy.easysign.global.jpaEnum.Gubun;
+
 public record CategoryResponse (
     Long categoryId,
-    String categoryName
+    String categoryName,
+
+    Gubun gubun,
+
+    int addSticker
+
 ) {}
--- Easysign_be/src/main/java/com/ssafy/easysign/sign/mapper/SignCategoryMapper.java
+++ Easysign_be/src/main/java/com/ssafy/easysign/sign/mapper/SignCategoryMapper.java
@@ -6,6 +6,5 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface SignCategoryMapper {
-
     CategoryResponse toCategoryResponse(SignCategory signCategory);
 }
