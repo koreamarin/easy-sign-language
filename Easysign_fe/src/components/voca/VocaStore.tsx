983723ce6c2b599d1040491df314af4f0d521232
서민주 <sminju1009@gmail.com>
2024-02-07 14:40:53 +0900

    fix: VocaStore.tsx 파일 백-프론트 연결 성공

--- Easysign_fe/src/components/voca/VocaStore.tsx
+++ Easysign_fe/src/components/voca/VocaStore.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+// 백-프론트 연결 관련 import
+import API from "../../config";
+import { useState, useEffect } from "react";
+
 // mui import
 // 이용하려는 것: https://mui.com/material-ui/react-card/#media
 
@@ -10,6 +14,37 @@ import React from "react";
 import { useInView } from "react-intersection-observer";
 
 function VocaStore() {
+  // 프론트 - 백 통신 설정
+  const [bookmark, setBookmark] = useState<any>([]);
+  // key는 추후 env 파일로 만들고 삭제 예정.
+
+  // 토큰을 로컬 스토리지에 저장
+  localStorage.setItem(
+    "token",
+    "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFYXN5U2lnbiIsImV4cCI6MTcwNzI5NDUwMiwiaWQiOjYsImxvZ2luSWQiOiJzc2FmeSJ9.hhi2vy9Kx0Uo2J617VLjV-3e10OVxhtAgIecFxrX1o2cfBCH1ZUd5MShJ2yk8WFfwcsy7FFoN0wsZ5hft5E5hg"
+  );
+
+  // 로컬 스토리지에서 토큰을 가져옴
+  const token = localStorage.getItem("token") || "";
+
+  const getBookmark = async () => {
+    const response = await fetch(`${API.BOOKMARK}`, {
+      method: "GET",
+      headers: {
+        Authorization: token,
+      },
+    });
+    const json = await response.json();
+    setBookmark(json);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getBookmark();
+  }, []);
+
+  // css 관련
+
   const Container = styled.div`
     min-height: 120vh;
     width: 70vw;
@@ -24,7 +59,15 @@ function VocaStore() {
   return (
     <Container>
       {/* 임시로 넣어놓은 모달 테스트 링크 */}
-      <Link to={"/store_test"}>모달 테스트</Link>;
+      <Link to={"/store_test"}>모달 테스트</Link>;<br></br>
+      {bookmark.map((item: any) => (
+        <div key={item.signId}>
+          <p>Sign ID: {item.signId}</p>
+          <p>Content: {item.content}</p>
+          <img src={item.imagePath} alt={item.content} />
+          {/* 비디오 등 추가적인 정보도 여기에 렌더링할 수 있음 */}
+        </div>
+      ))}
     </Container>
   );
 }
---
+++ Easysign_fe/src/config.js
@@ -0,0 +1,8 @@
+const BASE_URL = "https://i10c202.p.ssafy.io/api/v1";
+
+const API = {
+  // 단어장 조회
+  BOOKMARK: `${BASE_URL}/user/bookmark`,
+};
+
+export default API;
