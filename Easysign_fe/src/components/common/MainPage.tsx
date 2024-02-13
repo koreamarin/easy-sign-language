648310a1b8e372a41aadfd0158a1c70292738325
서민주 <sminju1009@gmail.com>
2024-02-13 13:58:29 +0900

    Design: 메인페이지 css 작업

--- Easysign_fe/src/components/common/MainPage.tsx
+++ Easysign_fe/src/components/common/MainPage.tsx
@@ -1,7 +1,71 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  height: 700px;
+  width: 100vw;
+  top: 40px;
+  background-color: #eee;
+`;
+
 function MainPage() {
   return (
     <div>
-      <h1>MainPage</h1>
+      <Container>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src="../mainpage_image/jihwa_practice.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="../mainpage_image/word_practice.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="../mainpage_image/sentence_practice.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="../mainpage_image/rain_game.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="../mainpage_image/line_game.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="../mainpage_image/speedquiz_game.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="../mainpage_image/catchmind_game.jpg" />
+          </SwiperSlide>
+        </Swiper>
+      </Container>
     </div>
   );
 }
