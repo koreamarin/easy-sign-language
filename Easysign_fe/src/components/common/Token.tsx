54aa022f80b0f57ea485546831c992399b9c98f2
서민주 <sminju1009@gmail.com>
2024-02-15 17:32:39 +0900

    fix: Token.tsx 파일 수정(토큰 받는 과정 오류 개선)

--- Easysign_fe/src/components/common/Token.tsx
+++ Easysign_fe/src/components/common/Token.tsx
@@ -1,10 +1,8 @@
 // 토큰 저장 페이지
 
-function Token() {
-  localStorage.setItem(
-    "token",
-    "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFYXN5U2lnbiIsImV4cCI6MTcwNzk3ODIzOCwiaWQiOjEsImxvZ2luSWQiOiJzc2FmeSJ9.8hLq-oy4_KjFTL7MZ_Fs94_i11Rhb7y163oYc2J7cBbMEnuTLbKNBz2FmuI_8xlfa-6UDWBB8lYOGHbUULneDw"
-  );
-}
+localStorage.setItem(
+  "token",
+  "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFYXN5U2lnbiIsImV4cCI6MTcwODA3MDM0MCwiaWQiOjEsImxvZ2luSWQiOiJzc2FmeSJ9.gbrEPzWL6cGNAj_SnFP4nQB5qmxrT72DD3y5et15w8lfG5iCkWJNrwnpih7ua9ulvpmF1GvolMwa9RNzEA4cvA"
+);
 
 export const token = localStorage.getItem("token") || "";
