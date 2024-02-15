fe0a9c504ee39c9b11db7f11a453863cd513fc22
SEO MINJU <sminju1009@gmail.com>
2024-02-15 21:37:37 +0900

    fix: api 추가

--- Easysign_fe/src/config.js
+++ Easysign_fe/src/config.js
@@ -1,5 +1,6 @@
 const BASE_URL = "https://easysign.shop/api/v1";
 
+// params를 받아야 하는 경우 여기에 적지 않는다.
 const API = {
   // 단어장 조회
   BOOKMARK: `${BASE_URL}/user/bookmark`,
@@ -11,6 +12,8 @@ const API = {
   STORE_LIST: `${BASE_URL}/store/info`,
   // 보유아이템
   ITEM_HAVE: `${BASE_URL}/user/items`,
+  // 아이템 구매
+  ITEM_PURCHASE: `${BASE_URL}/store/buyItem`,
 };
 
 export default API;
