ad488afffb88d5bb980e60bac822716bf5e97163
서민주 <sminju1009@gmail.com>
2024-02-26 14:16:05 +0900

    style: 토큰 저장 페이지 일부 수정

--- Easysign_fe/src/components/common/Token.tsx
+++ Easysign_fe/src/components/common/Token.tsx
@@ -2,7 +2,8 @@
 
 localStorage.setItem(
   "token",
-  "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFYXN5U2lnbiIsImV4cCI6MTcwODA3MDM0MCwiaWQiOjEsImxvZ2luSWQiOiJzc2FmeSJ9.gbrEPzWL6cGNAj_SnFP4nQB5qmxrT72DD3y5et15w8lfG5iCkWJNrwnpih7ua9ulvpmF1GvolMwa9RNzEA4cvA"
+  // TOKEN 부분에 토큰 정보 입력받음
+  "Bearer TOKEN"
 );
 
 export const token = localStorage.getItem("token") || "";
