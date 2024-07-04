4f0dc4640a327cb1575759b27830d269af39a9ad
koreamarin <awldnjs2@gmail.com>
2024-07-05 08:02:51 +0900

    Fix : fe-edu branch merge를 위한 main branch의 fe-edu 폴더 제거

--- Easysign_fe-edu/src/components/Sonagi/SonagiConfig.ts
+++
@@ -1,9 +0,0 @@
-export const LifeCount: number = 6;
-export const StaggerFrame: number = 100;
-export let SonagiLevel: number = 0;
-export const SonagiLevelUp = () => {
-  SonagiLevel += 1;
-};
-export const SonagiLevelReset = () => {
-  SonagiLevel = 0;
-};
--- Easysign_fe-edu/src/redux/store/store.tsx
+++
@@ -1,4 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "../modules/index";
-
-export default configureStore({ reducer: rootReducer });
